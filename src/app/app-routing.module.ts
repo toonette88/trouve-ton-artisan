4c3df714a735161ce6e696fc3623719a03e91b03
Amélie <amelie.adele@sfr.fr>
2024-06-27 16:10:02 +0200

    Ajout de la page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CategoryPageComponent } from './category-page/category-page.component';
 import { ArtisanPageComponent } from './artisan-page/artisan-page.component';
+import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
   {path: "category/:category", component: CategoryPageComponent},
   {path: "artisan/:id", component: ArtisanPageComponent},
+  {path: "**", pathMatch:'full', component: Page404Component},
   
 ];
 
