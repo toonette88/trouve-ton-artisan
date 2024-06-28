7f241dfdac73daf7ef5336b486dbcffe426139e7
Amélie <amelie.adele@sfr.fr>
2024-06-28 18:24:52 +0200

    Correctif 1 de la recherche

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,14 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent implements OnInit{
   artisans: any[] = [];
+  artisans1: any[] = [];
+  artisans2: any[] = [];
+  artisans3: any[] = [];
   search: string='';
 
  constructor(private route: ActivatedRoute, private artisansServices: ArtisansService, private router: Router) { }
 
  ngOnInit(): void {
    this.route.paramMap.subscribe(params => {
-    this.search=params.get('search')!;
-    this.artisans = this.artisansServices.getArtisansByLocation(this.search) || this.artisansServices.getArtisansByName(this.search) || this.artisansServices.getArtisansBySpecialty(this.search) ; 
+    this.search = params.get('search')!;
+    this.artisans1 = this.artisansServices.getArtisansByLocation(this.search);
+    this.artisans2 = this.artisansServices.getArtisansByName(this.search);
+    this.artisans3 = this.artisansServices.getArtisansBySpecialty(this.search); 
+    this.artisans = this.artisans1.concat(this.artisans2).concat(this.artisans3)
    });
  }
 }
